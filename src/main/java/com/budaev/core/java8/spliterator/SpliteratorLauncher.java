3b500cae8c8b59fdf97427b1dc79d3bd04f8ca5a
Ivan Budayeu <ivan_budayeu@epam.com>
2020-12-09 23:35:58 +0300

    SpliteratorLauncher provided

---
+++ src/main/java/com/budaev/core/java8/spliterator/SpliteratorLauncher.java
@@ -0,0 +1,46 @@
+package com.budaev.core.java8.spliterator;
+
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
+public class SpliteratorLauncher {
+
+	public static void main(String[] args) {
+
+		final List<String> words = Stream.generate(() -> "word").limit(20).collect(Collectors.toList());
+
+		System.out.println("Concurrent " + Spliterator.CONCURRENT);
+		System.out.println("Distinct " + Spliterator.DISTINCT);
+		System.out.println("Immutable " + Spliterator.IMMUTABLE);
+		System.out.println("Nonnull " + Spliterator.NONNULL);
+		System.out.println("Ordered " + Spliterator.ORDERED);
+		System.out.println("Sorted " + Spliterator.SORTED);
+		System.out.println("Subsized " + Spliterator.SUBSIZED);
+		System.out.println("Sized " + Spliterator.SIZED);
+
+		final Spliterator<String> spliterator = words.spliterator();
+		System.out.println(spliterator.characteristics());
+
+		final Spliterator<String> subSpliterator = spliterator.trySplit();
+
+		StringBuilder stringBuilder = new StringBuilder();
+		while (spliterator.tryAdvance(stringBuilder::append)) {
+			stringBuilder.append(' ');
+		}
+
+		StringBuilder subStringBuilder = new StringBuilder();
+		while (subSpliterator.tryAdvance(subStringBuilder::append)) {
+			subStringBuilder.append(' ');
+		}
+
+		System.out.println("First: " + stringBuilder.toString());
+		System.out.println("Second: " + subStringBuilder.toString());
+
+	}
+}
