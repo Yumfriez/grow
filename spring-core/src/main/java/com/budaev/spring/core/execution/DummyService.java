681fd8bbb9f9916ddd6681e59dd12e30a7c2b4fe
Ivan Budayeu <ivan_budayeu@epam.com>
2020-12-10 20:30:30 +0300

    Spring executors/schedulers example provided

---
+++ spring-core/src/main/java/com/budaev/spring/core/execution/DummyService.java
@@ -0,0 +1,22 @@
+package com.budaev.spring.core.execution;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
+@Service
+public class DummyService {
+
+	@Async
+	public void printThread() {
+		System.out.println(Thread.currentThread().getThreadGroup().getName() + " " + Thread.currentThread().getName());
+	}
+
+	@Scheduled(fixedRate = 500L)
+	public void scheduledPrint() {
+		System.out.println(Thread.currentThread().getThreadGroup().getName() + " " + Thread.currentThread().getName());
+	}
+}
