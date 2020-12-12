718a8a2570d6748eddb92c08fefcc474dda58f42
Ivan Budayeu <ivan_budayeu@epam.com>
2020-12-12 17:32:21 +0300

    Embedded tomcat example without spring boot provided

---
+++ spring-tomcat-embedded/src/main/java/com/budaev/spring/web/config/MyWebApplicationInitializer.java
@@ -0,0 +1,42 @@
+package com.budaev.spring.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.budaev.spring.web")
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+	/**
+	 * Register and configure all Servlet container components necessary to power the web application.
+	 */
+	@Override
+	public void onStartup(final ServletContext container) throws ServletException {
+		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+		rootContext.register(MyWebApplicationInitializer.class);
+		// rootContext.getEnvironment().setDefaultProfiles("default");
+
+		container.addListener(new ContextLoaderListener(rootContext));
+
+		AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+
+		ServletRegistration.Dynamic dispatcher = container
+				.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+	}
+
+}
---
+++ spring-tomcat-embedded/src/main/java/com/budaev/spring/web/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.budaev.spring.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+	@GetMapping
+	public String hello() {
+		return "hello";
+	}
+}
