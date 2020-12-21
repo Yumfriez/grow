7e25dd6aaa16c057ca346833b41733e7dc7502f4
Ivan Budayeu <ivan_budayeu@epam.com>
2020-12-21 23:20:48 +0300

    Custom event added

---
+++ spring-core/src/main/java/com/budaev/spring/core/ioc/annotation/event/CustomEvent.java
@@ -0,0 +1,22 @@
+package com.budaev.spring.core.ioc.annotation.event;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.ResolvableTypeProvider;
+
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
+public class CustomEvent<T> extends ApplicationEvent implements ResolvableTypeProvider {
+
+	private T source;
+
+	public CustomEvent(T source) {
+		super(source);
+	}
+
+	@Override
+	public ResolvableType getResolvableType() {
+		return ResolvableType.forClassWithGenerics(this.getClass(), ResolvableType.forInstance(source));
+	}
+}
